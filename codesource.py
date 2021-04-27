9d0f9d897a14d3ec0257df72aaa8e57e4733bad1
npaillasson <nathanpaillasson3@gmail.com>
2021-04-28 01:27:08 +0200

    the number of available books is extract from the primary string

--- codesource.py
+++ codesource.py
@@ -1,16 +1,20 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 
-headers = "product_page_url; universal_ product_code (upc); title; price_including_tax; " \
+AVAILABLE_EXTRACT_EXPRESSION = re.compile(r"\d+")  \
+    # regex to extract the number of books available from the primary data
+
+HEADERS = "product_page_url; universal_ product_code (upc); title; price_including_tax; " \
           "price_excluding_tax; number_available; product_description; category; review_rating; image_url" \
-    # column headers
+    # column HEADERS
 
-img_link = "http://books.toscrape.com/"
+IMG_LINK = "http://books.toscrape.com/"
 
-rate_dict = {"One": "1", "Two": "2", "Three": "3", "Four": "4", "Five": "5"} \
-    # dict to convert numbers in letter in an alphanumeric value
+RATE_DICT = {"One": "1", "Two": "2", "Three": "3", "Four": "4", "Five": "5"} \
+    # dict to convert numbers in letter in a numeric value
 
 response = requests.get(url)
 
@@ -23,13 +27,13 @@ if response.ok:
     category = str(category[2].get_text()).strip()
     product_description = soup.findAll("p")
     rate = product_description[2]["class"][1]
-    rate = rate_dict[rate]
+    rate = RATE_DICT[rate]
     product_description = str(product_description[3].get_text())
     img = soup.find("img")
-    img = img["src"].replace("../../", img_link)
+    img = img["src"].replace("../../", IMG_LINK)
     tds = soup.findAll("td")
 
-    infos_list = [url]  # list who contains all data sorted in order about the book
+    infos_list = [url]  # list who contains all data about the book (sorted in order)
 
     for td in tds:  # loop who get data from the information tab and add them in the infos_list
         td = str(td.get_text())
@@ -41,9 +45,11 @@ if response.ok:
     infos_list.insert(7, category)
     infos_list[8] = rate
     infos_list.append(img)
+    
     infos_list[3], infos_list[4] = infos_list[3][1:], infos_list[4][1:]  # fix the encoding issue on the price fields
+    infos_list[5] = AVAILABLE_EXTRACT_EXPRESSION.findall(infos_list[5])[0]
     infos = ";".join(infos_list)  # convert infos_list in str using ";" as separator before writing it in cvs file
 
     with open("myFile.csv", "w") as file:
-        file.write(headers)
+        file.write(HEADERS)
         file.write("\n" + infos)
