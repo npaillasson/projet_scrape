23a3ae664dd35a4cd039b15f89a358dc36ff5daf
npaillasson <nathanpaillasson3@gmail.com>
2021-04-27 16:30:52 +0200

    translation of comments in English, and PEP compliant code

--- codesource.py
+++ codesource.py
@@ -3,45 +3,47 @@ from bs4 import BeautifulSoup
 
 url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 
-productInformations = "product_page_url; universal_ product_code (upc); title; price_including_tax; " \
-                      "price_excluding_tax; number_available; product_description; category; review_rating; image_url" #en-têtes des colonnes
+headers = "product_page_url; universal_ product_code (upc); title; price_including_tax; " \
+          "price_excluding_tax; number_available; product_description; category; review_rating; image_url" \
+    # column headers
 
-img_link="http://books.toscrape.com/"
+img_link = "http://books.toscrape.com/"
 
-rate_dict={"One" : "1", "Two" : "2", "Three" : "3", "Four" : "4", "Five" : "5"} #dictionnaire qui permet de convertir la note extraite du site en chiffre
+rate_dict = {"One": "1", "Two": "2", "Three": "3", "Four": "4", "Five": "5"} \
+    # dict to convert numbers in letter in an alphanumeric value
 
 response = requests.get(url)
 
 if response.ok:
     soup = BeautifulSoup(response.text, "html.parser")
 
-    bookTitle = soup.find("h1")
-    bookTitle = str(bookTitle.get_text())
+    book_title = soup.find("h1")
+    book_title = str(book_title.get_text())
     category = soup.findAll("li")
     category = str(category[2].get_text()).strip()
-    productDescription = soup.findAll("p")
-    rate = productDescription[2]["class"][1]
-    rate=rate_dict[rate]
-    productDescription = str(productDescription[3].get_text())
+    product_description = soup.findAll("p")
+    rate = product_description[2]["class"][1]
+    rate = rate_dict[rate]
+    product_description = str(product_description[3].get_text())
     img = soup.find("img")
-    img = img["src"].replace("../../",img_link)
+    img = img["src"].replace("../../", img_link)
     tds = soup.findAll("td")
 
-    infosList = [url] #liste qui contiendra dans l'ordre toutes les données des différentes colonnes
+    infos_list = [url]  # list who contains all data sorted in order about the book
 
-    for td in tds: #boucle qui permet de récupérer les données du tableau d'information du produit et de les ajouter dans infosList
-        td=str(td.get_text())
-        infosList.append(td)
-    del infosList[2] #suppression de la donnée "Books"
-    del infosList[4] #suppression de la donnée "montant de la taxe"
-    infosList.insert(2,bookTitle)
-    infosList.insert(6,productDescription)
-    infosList.insert(7,category)
-    infosList[8] = rate
-    infosList.append(img)
-    infosList[3], infosList[4]= infosList[3][1:], infosList[4][1:] #correction provisoire au problème d'encodage sur le prix
-    infos = ";".join(infosList) #conversion d'infosList de "list" à "str" avec ";" comme séparateur avant écriture dans fichier csv
+    for td in tds:  # loop who get data from the information tab and add them in the infos_list
+        td = str(td.get_text())
+        infos_list.append(td)
+    del infos_list[2]  # remove the field "Books"
+    del infos_list[4]  # remove the field "tax"
+    infos_list.insert(2, book_title)
+    infos_list.insert(6, product_description)
+    infos_list.insert(7, category)
+    infos_list[8] = rate
+    infos_list.append(img)
+    infos_list[3], infos_list[4] = infos_list[3][1:], infos_list[4][1:]  # fix the encoding issue on the price fields
+    infos = ";".join(infos_list)  # convert infos_list in str using ";" as separator before writing it in cvs file
 
     with open("myFile.csv", "w") as file:
-        file.write(productInformations)
+        file.write(headers)
         file.write("\n" + infos)
