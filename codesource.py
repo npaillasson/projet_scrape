4af93e5b0a74be11eaf0e88868597c6bd5d65419
npaillasson <nathanpaillasson3@gmail.com>
2021-04-28 15:36:20 +0200

    comments correction and variable names modification

--- codesource.py
+++ codesource.py
@@ -8,8 +8,8 @@ AVAILABLE_EXTRACT_EXPRESSION = re.compile(r"\d+")  \
     # regex to extract the number of books available from the primary data
 
 HEADERS = "product_page_url; universal_ product_code (upc); title; price_including_tax; " \
-          "price_excluding_tax; number_available; product_description; category; review_rating; image_url" \
-    # column HEADERS
+          "price_excluding_tax; number_available; product_description; category;" \
+          " review_rating; image_url"  # column HEADERS
 
 IMG_LINK = "http://books.toscrape.com/"
 
@@ -31,25 +31,28 @@ if response.ok:
     product_description = str(product_description[3].get_text())
     img = soup.find("img")
     img = img["src"].replace("../../", IMG_LINK)
-    tds = soup.findAll("td")
+    information_table = soup.findAll("td")
 
-    infos_list = [url]  # list who contains all data about the book (sorted in order)
+    info_list = [url]  # list who contains all data about the book (sorted in order)
 
-    for td in tds:  # loop who get data from the information tab and add them in the infos_list
-        td = str(td.get_text())
-        infos_list.append(td)
-    del infos_list[2]  # remove the field "Books"
-    del infos_list[4]  # remove the field "tax"
-    infos_list.insert(2, book_title)
-    infos_list.insert(6, product_description)
-    infos_list.insert(7, category)
-    infos_list[8] = rate
-    infos_list.append(img)
+    for information_line in information_table:  \
+            # loop who get data from the information tab and add them in the info_list
+        information_line = str(information_line.get_text())
+        info_list.append(information_line)
+    del info_list[2]  # remove the field "Books"
+    del info_list[4]  # remove the field "tax"
+    info_list.insert(2, book_title)
+    info_list.insert(6, product_description)
+    info_list.insert(7, category)
+    info_list[8] = rate
+    info_list.append(img)
     
-    infos_list[3], infos_list[4] = infos_list[3][1:], infos_list[4][1:]  # fix the encoding issue on the price fields
-    infos_list[5] = AVAILABLE_EXTRACT_EXPRESSION.findall(infos_list[5])[0]
-    infos = ";".join(infos_list)  # convert infos_list in str using ";" as separator before writing it in cvs file
+    info_list[3], info_list[4] = info_list[3][1:], info_list[4][1:]
+    # fix the encoding issue on the price fields
+    info_list[5] = AVAILABLE_EXTRACT_EXPRESSION.findall(info_list[5])[0]
+    info = ";".join(info_list)
+    # convert info_list in str using ";" as separator before writing it in cvs file
 
     with open("myFile.csv", "w") as file:
         file.write(HEADERS)
-        file.write("\n" + infos)
+        file.write("\n" + info)
