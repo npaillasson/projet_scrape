00a931cb572fca4ed895e753b2b9ec291663a38a
npaillasson <nathanpaillasson3@gmail.com>
2021-04-30 15:06:35 +0200

    codesource.py optimization

--- codesource.py
+++ codesource.py
@@ -7,27 +7,24 @@ import os
 from bs4 import BeautifulSoup
 from math import ceil
 from functions import *
+import time
+
+depart = time.time()
 
 if not os.path.exists("results/"):  # creation of results directory if it doesn't already exist
     os.mkdir("results")
 
-SPACE_DELETION_EXPRESSION = re.compile(r" ")
-
-RESULTS_NUMBER_EXTRACT_EXPRESSION = re.compile(r"\d+")\
-    # regex used to extract the number of books in one category
+EXTRACT_NUMBER_EXPRESSION = re.compile(r"\d+")\
+    # regex used to extract the number of books in one category and the number of items available
 
 URL_INDEX_EXTRACT_EXPRESSION = re.compile(r"index.html$")\
     # regex used to create the concerned category next page's url
 # when category contains more than 20 books
 
-AVAILABLE_EXTRACT_EXPRESSION = re.compile(r"\d+")  \
-    # regex to extract the number of books available from the primary data
 
 WRONG_SEPARATORS_DELETION_EXPRESSION = re.compile(r";")
 
 ENCODE_ISSUES_DELETION_EXPRESSION = re.compile(r"[âÃ©]")
-
-BASE_IMG_URL = "http://books.toscrape.com/"
 
 BASE_CATEGORY_URL = "http://books.toscrape.com/catalogue/category/books/" \
     # common part of category's links
@@ -41,28 +38,23 @@ HEADERS = "product_page_url; universal_ product_code (upc); title; price_includi
           "price_excluding_tax; number_available; product_description; category;" \
           " review_rating; image_url"  # columns HEADERS
 
-target_url = "http://books.toscrape.com/"
+TARGET_URL = "http://books.toscrape.com/"
 
-main_request = requests.get(target_url)
+main_request = requests.get(TARGET_URL)
 
 if main_request.ok:
 
     main_soup = BeautifulSoup(main_request.text, "html.parser")
     category_url_list = []
-    category_name_list = []
-    category_name_extraction = main_soup.find("aside")
-    category_name_extraction = category_name_extraction.findAll("ul")
-    category_name_extraction = category_name_extraction[1]
-    category_name_extraction = category_name_extraction.findAll("li")
-    for category_name in category_name_extraction:
-        category_name = category_name.get_text().strip()
-        category_name = SPACE_DELETION_EXPRESSION.sub("-", category_name)
-        category_name_list.append(category_name)
-
-    for position, category_name in enumerate(category_name_list):
-        category_url = "{}{}_{}/index.html".format(BASE_CATEGORY_URL, category_name.lower(), position+2)
+    category_url_extraction = main_soup.find("aside")
+    category_url_extraction = category_url_extraction.findAll("ul")
+    category_url_extraction = category_url_extraction[1]
+    category_url_extraction = category_url_extraction.findAll("li")
+    for category_name in category_url_extraction:
+        category_url = category_name.find("a")
+        category_url = category_url["href"]
+        category_url = '{}{}'.format(TARGET_URL, category_url)
         category_url_list.append(category_url)
-        print(category_url)
 
     for category_url in category_url_list:
 
@@ -75,7 +67,7 @@ if main_request.ok:
             category_soup = BeautifulSoup(category_request.text, "html.parser")
             category_name = category_soup.find("h1").get_text()
             page_number = category_soup.findAll("form")
-            page_number = int(RESULTS_NUMBER_EXTRACT_EXPRESSION.findall(page_number[0].get_text())[0]) \
+            page_number = int(EXTRACT_NUMBER_EXPRESSION.findall(page_number[0].get_text())[0]) \
                 # extraction of the amount of books in the category (using regex)
             page_number = ceil(page_number/20)  # determination of the number of pages in the category
             page_counter = 0  # counter used in the while loop
@@ -85,10 +77,10 @@ if main_request.ok:
                 if page_counter == 0:
                     books_page_url_list.extend(book_page_url_extractor(category_soup, BASE_BOOK_URL))
                 else:
-                    iter_link_category = "{}page-{}{}".format(base_link_category, (page_counter + 1), ".html")
-                    iter_requests_category = requests.get(iter_link_category)
-                    iter_soup_category = BeautifulSoup(iter_requests_category.text, "html.parser")
-                    books_page_url_list.extend(book_page_url_extractor(iter_soup_category, BASE_BOOK_URL))
+                    iter_link_category = "{}/page-{}.html".format(BASE_CATEGORY_URL, (page_counter + 1))
+                    iter_category_request = requests.get(iter_link_category)
+                    iter_category_soup = BeautifulSoup(iter_category_request.text, "html.parser")
+                    books_page_url_list.extend(book_page_url_extractor(iter_category_soup, BASE_BOOK_URL))
 
                 page_counter += 1
 
@@ -115,7 +107,7 @@ if main_request.ok:
                             # replace ";" with ":" in the product description paragraph
                         product_description = ENCODE_ISSUES_DELETION_EXPRESSION.sub("", product_description)
                         img = book_soup.find("img")
-                        img = img["src"].replace("../../", BASE_IMG_URL)
+                        img = img["src"].replace("../../", TARGET_URL)
                         information_table = book_soup.findAll("td")
 
                         info_list = [book_page_url]\
@@ -135,9 +127,11 @@ if main_request.ok:
 
                         info_list[3], info_list[4] = info_list[3][1:], info_list[4][1:]
                         # fix the encoding issue on the price fields
-                        info_list[5] = AVAILABLE_EXTRACT_EXPRESSION.findall(info_list[5])[0]
+                        info_list[5] = EXTRACT_NUMBER_EXPRESSION.findall(info_list[5])[0]
                         # extraction of the number of books available
                         info = ";".join(info_list)
                         # convert info_list in str using ";" as separator before writing it in cvs file
 
                         file.write("\n" + info)
+arrive = time.time()
+print(arrive-depart)
