8150d02501d92f055f0a104182f5b7de39afa575
npaillasson <nathanpaillasson3@gmail.com>
2021-04-30 12:40:28 +0200

    update codesource.py and functions.py, the scrapers bot is functional

--- codesource.py
+++ codesource.py
@@ -11,12 +11,12 @@ from functions import *
 if not os.path.exists("results/"):  # creation of results directory if it doesn't already exist
     os.mkdir("results")
 
-working_url = "http://books.toscrape.com/catalogue/category/books/fantasy_19/index.html"
+SPACE_DELETION_EXPRESSION = re.compile(r" ")
 
 RESULTS_NUMBER_EXTRACT_EXPRESSION = re.compile(r"\d+")\
     # regex used to extract the number of books in one category
 
-LINK_INDEX_EXTRACT_EXPRESSION = re.compile(r"index.html$")\
+URL_INDEX_EXTRACT_EXPRESSION = re.compile(r"index.html$")\
     # regex used to create the concerned category next page's url
 # when category contains more than 20 books
 
@@ -27,7 +27,7 @@ WRONG_SEPARATORS_DELETION_EXPRESSION = re.compile(r";")
 
 ENCODE_ISSUES_DELETION_EXPRESSION = re.compile(r"[âÃ©]")
 
-BASE_IMG_LINK = "http://books.toscrape.com/"
+BASE_IMG_URL = "http://books.toscrape.com/"
 
 BASE_CATEGORY_URL = "http://books.toscrape.com/catalogue/category/books/" \
     # common part of category's links
@@ -41,78 +41,103 @@ HEADERS = "product_page_url; universal_ product_code (upc); title; price_includi
           "price_excluding_tax; number_available; product_description; category;" \
           " review_rating; image_url"  # columns HEADERS
 
-base_link_category = LINK_INDEX_EXTRACT_EXPRESSION.sub("", working_url)
+target_url = "http://books.toscrape.com/"
 
-category_requests = requests.get(working_url)
+main_request = requests.get(target_url)
 
-if category_requests.ok:
+if main_request.ok:
 
-    category_soup = BeautifulSoup(category_requests.text, "html.parser")
-    category_name = category_soup.find("h1").get_text()
-    page_number = category_soup.findAll("form")
-    page_number = int(RESULTS_NUMBER_EXTRACT_EXPRESSION.findall(page_number[0].get_text())[0]) \
-        # extraction of the amount of books in the category (using regex)
-    page_number = ceil(page_number/20)  # determination of the number of pages in the category
-    page_counter = 0  # counter used in the while loop
-    books_page_url_list = []
+    main_soup = BeautifulSoup(main_request.text, "html.parser")
+    category_url_list = []
+    category_name_list = []
+    category_name_extraction = main_soup.find("aside")
+    category_name_extraction = category_name_extraction.findAll("ul")
+    category_name_extraction = category_name_extraction[1]
+    category_name_extraction = category_name_extraction.findAll("li")
+    for category_name in category_name_extraction:
+        category_name = category_name.get_text().strip()
+        category_name = SPACE_DELETION_EXPRESSION.sub("-", category_name)
+        category_name_list.append(category_name)
 
-    while page_counter != page_number:
-        if page_counter == 0:
-            books_page_url_list.extend(book_page_url_extractor(category_soup, BASE_BOOK_URL))
-        else:
-            iter_link_category = "{}page-{}{}".format(base_link_category, (page_counter + 1), ".html")
-            iter_requests_category = requests.get(iter_link_category)
-            iter_soup_category = BeautifulSoup(iter_requests_category.text, "html.parser")
-            books_page_url_list.extend(book_page_url_extractor(iter_soup_category, BASE_BOOK_URL))
+    for position, category_name in enumerate(category_name_list):
+        category_url = "{}{}_{}/index.html".format(BASE_CATEGORY_URL, category_name.lower(), position+2)
+        category_url_list.append(category_url)
+        print(category_url)
 
-        page_counter += 1
+    for category_url in category_url_list:
 
-with open("results/{}.csv".format(category_name), "w") as file:
-    file.write(HEADERS)
+        base_link_category = URL_INDEX_EXTRACT_EXPRESSION.sub("", category_url)
 
-    for book_page_url in books_page_url_list:
+        category_request = requests.get(category_url)
 
-        # data extraction from books pages
+        if category_request.ok:
 
-        response = requests.get(book_page_url)
+            category_soup = BeautifulSoup(category_request.text, "html.parser")
+            category_name = category_soup.find("h1").get_text()
+            page_number = category_soup.findAll("form")
+            page_number = int(RESULTS_NUMBER_EXTRACT_EXPRESSION.findall(page_number[0].get_text())[0]) \
+                # extraction of the amount of books in the category (using regex)
+            page_number = ceil(page_number/20)  # determination of the number of pages in the category
+            page_counter = 0  # counter used in the while loop
+            books_page_url_list = []
 
-        if response.ok:
-            book_soup = BeautifulSoup(response.text, "html.parser")
+            while page_counter != page_number:
+                if page_counter == 0:
+                    books_page_url_list.extend(book_page_url_extractor(category_soup, BASE_BOOK_URL))
+                else:
+                    iter_link_category = "{}page-{}{}".format(base_link_category, (page_counter + 1), ".html")
+                    iter_requests_category = requests.get(iter_link_category)
+                    iter_soup_category = BeautifulSoup(iter_requests_category.text, "html.parser")
+                    books_page_url_list.extend(book_page_url_extractor(iter_soup_category, BASE_BOOK_URL))
 
-            book_title = book_soup.find("h1").get_text()
-            category = book_soup.findAll("li")
-            category = str(category[2].get_text()).strip()
-            rate_and_product_description = book_soup.findAll("p")
-            rate = rate_and_product_description[2]["class"][1]
-            rate = RATE_DICT[rate]
-            product_description = str(rate_and_product_description[3].get_text())
-            product_description = WRONG_SEPARATORS_DELETION_EXPRESSION.sub(":", product_description)\
-                # replace ";" with ":" in the product description paragraph
-            product_description = ENCODE_ISSUES_DELETION_EXPRESSION.sub("", product_description)
-            img = book_soup.find("img")
-            img = img["src"].replace("../../", BASE_IMG_LINK)
-            information_table = book_soup.findAll("td")
+                page_counter += 1
 
-            info_list = [book_page_url]\
-                # list who contains all data about the book (sorted in order)
+            with open("results/{}.csv".format(category_name), "w") as file:
+                file.write(HEADERS)
 
-            for information_line in information_table:  \
-                    # loop who gets data from the information tab and add them in the info_list
-                information_line = str(information_line.get_text())
-                info_list.append(information_line)
-            del info_list[2]  # remove the field "Books"
-            del info_list[4]  # remove the field "tax"
-            info_list.insert(2, book_title)
-            info_list.insert(6, product_description)
-            info_list.insert(7, category)
-            info_list[8] = rate
-            info_list.append(img)
+                for book_page_url in books_page_url_list:
 
-            info_list[3], info_list[4] = info_list[3][1:], info_list[4][1:]
-            # fix the encoding issue on the price fields
-            info_list[5] = AVAILABLE_EXTRACT_EXPRESSION.findall(info_list[5])[0]
-            # extraction of the number of books available
-            info = ";".join(info_list)
-            # convert info_list in str using ";" as separator before writing it in cvs file
+                    # data extraction from books pages
 
-            file.write("\n" + info)
+                    book_request = requests.get(book_page_url)
+
+                    if book_request.ok:
+                        book_soup = BeautifulSoup(book_request.text, "html.parser")
+
+                        book_title = book_soup.find("h1").get_text()
+                        category = book_soup.findAll("li")
+                        category = str(category[2].get_text()).strip()
+                        rate_and_product_description = book_soup.findAll("p")
+                        rate = rate_and_product_description[2]["class"][1]
+                        rate = RATE_DICT[rate]
+                        product_description = str(rate_and_product_description[3].get_text())
+                        product_description = WRONG_SEPARATORS_DELETION_EXPRESSION.sub(":", product_description)\
+                            # replace ";" with ":" in the product description paragraph
+                        product_description = ENCODE_ISSUES_DELETION_EXPRESSION.sub("", product_description)
+                        img = book_soup.find("img")
+                        img = img["src"].replace("../../", BASE_IMG_URL)
+                        information_table = book_soup.findAll("td")
+
+                        info_list = [book_page_url]\
+                            # list who contains all data about the book (sorted in order)
+
+                        for information_line in information_table:  \
+                                # loop who gets data from the information tab and add them in the info_list
+                            information_line = str(information_line.get_text())
+                            info_list.append(information_line)
+                        del info_list[2]  # remove the field "Books"
+                        del info_list[4]  # remove the field "tax"
+                        info_list.insert(2, book_title)
+                        info_list.insert(6, product_description)
+                        info_list.insert(7, category)
+                        info_list[8] = rate
+                        info_list.append(img)
+
+                        info_list[3], info_list[4] = info_list[3][1:], info_list[4][1:]
+                        # fix the encoding issue on the price fields
+                        info_list[5] = AVAILABLE_EXTRACT_EXPRESSION.findall(info_list[5])[0]
+                        # extraction of the number of books available
+                        info = ";".join(info_list)
+                        # convert info_list in str using ";" as separator before writing it in cvs file
+
+                        file.write("\n" + info)
--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
-def book_page_url_extractor(specific_soup, BASE_BOOK_URL):
+def book_page_url_extractor(specific_soup, base_url):
     """Function who returns a list who contains all books links of the page"""
     result_list = []
     items_list = specific_soup.findAll("article")
     for item in items_list:
         link_page = item.find("a")
         link_page = str(link_page["href"])
-        link_page = link_page.replace("../../../", BASE_BOOK_URL)
+        link_page = link_page.replace("../../../", base_url)
         result_list.append(link_page)
     return result_list
